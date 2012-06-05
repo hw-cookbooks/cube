cf5a477cfbac3bd19800f8e681def3b25e30ee01
Trotter Cashion <cashion@gmail.com>
2012-06-05 10:57:36 -0700

    Add redhat and variants to supported list

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,9 @@ description      "Installs/Configures cube"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.4.0"
 
-supports "ubuntu"
+%w( centos redhat fedora ubuntu ).each do |os|
+  supports "os"
+end
 
 depends "mongodb"
 depends "nodejs"
